2a841ec663704700f5de8a45b8e4da7048018cf6
Oğuzhan Turgut <oguzhanturgut611@gmail.com>
2024-05-01 20:30:14 +0300

    init

---
+++ server/server.Application/Services/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using server.Application.Features.Auth.Login;
+using server.Domain.Entities;
+
+namespace server.Application.Services;
+
+public interface IJwtProvider
+{
+    Task<LoginCommandResponse> CreateToken(AppUser user);
+}
